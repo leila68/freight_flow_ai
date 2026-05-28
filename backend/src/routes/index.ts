da9b3e3ab7041329e2d97c7c06f9b5671efaba63
Leila <leila.cheshmi@gmail.com>
2026-05-28 10:12:18 -0400

    add equipment route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,6 +3,7 @@ import healthRouter from './health';
 import lanesRouter  from './lanes';
 import quotesRouter from './quotes';
 import accessorialsRouter from './accessorials';
+import equipmentMultipliersRouter from './equipmentMultipliers'
 
 const router = Router();
 
@@ -12,5 +13,6 @@ router.use('/health',  healthRouter);
 router.use('/api/lanes',  lanesRouter);
 router.use('/api/quotes', quotesRouter);
 router.use('/api/accessorials', accessorialsRouter)
+router.use('/api/equipment-multipliers', equipmentMultipliersRouter)
 
 export default router;
